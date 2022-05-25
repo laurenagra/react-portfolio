59bd7372e0f94f26f0e3df2d906165971e616f69
Lauren A <laurenhaileyama@gmail.com>
2022-05-25 16:43:55 -0400

    import images for footer links and fill out some basic code in about/home and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,55 @@
 /*Import icon links for github, linked in, and twitter */ 
-import React from 'react';
+// import React from 'react';
 
-const Footer = () => { 
-    return(
-        <footer className="w-100 mt-auto text-dark p-4">
-            <div className="container text-center mb-5">
-                <h4></h4>
-            </div>
-        </footer>
-    )
+// const Footer = () => { 
+//     return(
+//         <footer className="w-100 mt-auto text-dark p-4">
+//             <div className="container text-center mb-5">
+//                 <h4></h4>
+//             </div>
+//         </footer>
+//     )
+// }
+
+import React from "react";
+import "../styles/Footer.css";
+import githubPic from "../assets/images/github.png";
+import linkedinPic from "../assets/images/linkedin.png";
+import twitterPic from "../assets/images/twitter.png";
+import gmailPic from "../assets/images/gmail.png";
+
+export default function Footer() {
+  return (
+    <footer className="footer-links">
+      <a href="https://github.com/laurenagra" rel="noreferrer" target="_blank">
+        <img
+          className="linkPic"
+          src={githubPic}
+          alt="link to Sunny's Github"
+        />
+      </a>
+
+      <a href="www.linkedin.com/in/sunny-agra" rel="noreferrer" target="_blank">
+        <img
+          className="linkPic"
+          src={linkedinPic}
+          alt="link to Sunny's LinkedIn Profile"
+        />
+      </a>
+      <a href="mailto:laurenhaileyama@gmail.com" rel="noreferrer" target="_blank">
+        <img
+          className="linkPic"
+          src={gmailPic}
+          alt="link to Sunny's Email"
+        />
+      </a>
+      <a href="https://twitter.com/sunnylinguxst" rel="noreferrer" target="_blank">
+        <img
+          className="linkPic"
+          src={twitterPic}
+          alt="link to Sunny's Twitter Profile"
+        />
+      </a>
+    </footer>
+  );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,2 +1,4 @@
 //tie in header, footer, and about 
 // on initial page load, load the About Me page 
+import Nav from '../Nav';
+import About from './About'
